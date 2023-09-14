90742ea9c09fbf6c1dae2bdbf43db764864d85d7
DatNguyen2002 <90488040+DatNguyen2002@users.noreply.github.com>
2023-09-14 16:24:33 +0700

    Update 13/9/2023

---
+++ Knight Adventure/Assets/Scrip/PlayerMovement.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    private float moving = 0f;
+    private float speed = 10f;
+    private float jumpForce = 10f;
+    private bool isFacingRight = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Flip();
+    }
+
+    private void Flip()
+    {
+        ///<summary>
+        ///Neu nhan vat (quay mat sang phai && nhan di chuyen sang trai) 
+        ///             se thay doi Scale de Player quay mat sang trai
+        ///Neu nhan vat ( quay mat sang trai && nhan di chuyern sang phai)
+        ///             se thay doi Scale de Player quay mat sang phai
+        /// </summary>
+        if((isFacingRight && moving< 0f)||(!isFacingRight && moving > 0f))
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale=localScale;
+        }
+    }
+
+    private void Move()
+    {
+        moving = Input.GetAxisRaw("Horizontal");//nhan gia tri dau vao tu ban phim
+        rb.velocity = new Vector2(moving * speed, rb.velocity.y);// su ly di chuyen theo chieu x
+
+        //su ly nhay theo chieu y
+        if (Input.GetButtonDown("Jump"))// neu nut dc an thi se thay doi chieu cao nhan vat
+        {
+            rb.velocity =new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Kinght
-12/9/2023 Create empty project
+12/9/2023: Create empty project
+
+13/9/2023: Moving for Player (incomplete)
+
+13/9/2023: 
+
+- Moving for player (Complete)
+
+- Animation
